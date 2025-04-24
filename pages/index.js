b23b15da592f7282b223ff5d1903786d2569d5a3
Samet <sametanaz.tr@gmail.com>
2025-04-24 10:11:10 +0300

    visual improvements

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 // pages/index.js
 import React from "react";
 import Link from "next/link";
-import styles from "../styles/home.module.css";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <>
+    <main className={styles.container}>
       {/* Hero Bölümü */}
       <section className={styles.hero}>
         <h1>Samet Anaz</h1>
@@ -16,7 +16,7 @@ export default function Home() {
       </section>
 
       {/* Blog Giriş Bölümü */}
-      <section className={`${styles.blogIntro} ${styles.container}`}>
+      <section className={styles.blogIntro}>
         <h2>Bloglarım</h2>
         <article className={styles.postPreview}>
           <h3>
@@ -29,13 +29,14 @@ export default function Home() {
             24 Nisan 2025
           </time>
           <p className={styles.postExcerpt}>
-            Merhaba! Ben Samet, şu anda üniversite 3. sınıf Bilgisayar Mühendisliği öğrencisiyim. Bu
-            yazıda sizlerle hem kendimi nasıl geliştirdiğimi hem de bu süreçte edindiğim bazı deneyimleri
-            paylaşmak istiyorum. Belki benim yolculuğum, bu alanda ilerlemek isteyen başkalarına da
-            ilham olur.
+            Merhaba! Ben Samet, şu anda üniversite 3. sınıf Bilgisayar Mühendisliği öğrencisiyim.
+            Bu yazıda sizlerle hem kendimi nasıl geliştirdiğimi hem de bu süreçte edindiğim bazı
+            deneyimleri paylaşmak istiyorum. Belki benim yolculuğum, bu alanda ilerlemek isteyen
+            başkalarına da ilham olur...
+            <Link href="/blog/blog-yazi-1">Devamını Oku</Link>
           </p>
         </article>
       </section>
-    </>
+    </main>
   );
 }
