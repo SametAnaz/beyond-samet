df3dd0de162344c485e398dc93429d55509a54e8
Samet <sametanaz.tr@gmail.com>
2025-04-25 11:23:32 +0300

    Dark theme added

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,21 @@
 
 import "@/styles/globals.css";
 import Layout from "../src/components/Layout";
+import { ThemeProvider } from "next-themes";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <SpeedInsights />
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem={true}
+      enableColorScheme={true}
+    >
+      <Layout>
+        <SpeedInsights />
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 // src/components/Navbar.js
 import React from "react";
 import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
 
 export default function Navbar() {
   return (
@@ -10,6 +11,7 @@ export default function Navbar() {
   <li><Link href="/blog">Blog</Link></li>
   <li><Link href="/about">Hakkımda</Link></li>
   <li><Link href="/contact">İletişim</Link></li>
+  <li><ThemeToggle /></li>
 </ul>
     </nav>
   );
