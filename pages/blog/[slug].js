4c1e8bd85b05f6427224e738493a29bae6014e7b
Samet <sametanaz.tr@gmail.com>
2025-04-25 13:06:41 +0300

    bug fix & visual update

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,32 +2,27 @@
 
 import React from "react";
 import Head from "next/head";
+import Giscus from "@giscus/react";               // ← default import
 import styles from "../../styles/blog-post.module.css";
 import { getAllPostSlugs, getPostData } from "../../lib/posts";
-import GiscusComments from "../../src/components/GiscusComments";
 
 export async function getStaticPaths() {
   const paths = getAllPostSlugs();
-  return {
-    paths,
-    fallback: false,
-  };
+  return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.slug);
-  return {
-    props: { postData },
-  };
+  return { props: { postData } };
 }
 
 export default function Post({ postData }) {
   return (
     <>
       <Head>
-        {/* Use a single template string so <title> has a single child */}
         <title>{`${postData.title} – Beyond Samet`}</title>
       </Head>
+
       <article className={styles.article}>
         <h1 className={styles.title}>{postData.title}</h1>
         <div className={styles.date}>
@@ -41,14 +36,23 @@ export default function Post({ postData }) {
           className={styles.content}
           dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
         />
-        {/* Giscus Comments */}
-        <GiscusComments
+      </article>
+
+      {/* Giscus ile Yorumlar */}
+      <div className={styles.comments}>
+        <Giscus
           repo="SametAnaz/beyond-samet"
           repoId="971767619"
           category="General"
           categoryId="44421397"
+          mapping="pathname"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="bottom"
+          theme="dark_dimmed"
+          lang="tr"
         />
-      </article>
+      </div>
     </>
   );
 }
