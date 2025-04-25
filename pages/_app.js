7427742003a723b749020b223a7402f4e3f683c2
Samet <sametanaz.tr@gmail.com>
2025-04-25 06:01:02 +0300

    add Layout & SpeedInsights

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+// pages/_app.js
+
 import "@/styles/globals.css";
 import Layout from "../src/components/Layout";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <SpeedInsights />
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
